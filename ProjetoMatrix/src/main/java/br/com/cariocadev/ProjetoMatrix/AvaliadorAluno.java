508b1c7356a3d9a4b32d161a66ab8c1bbb8c6407
GuilhermeSHB <41237456+GuilhermeSHB@users.noreply.github.com>
2018-08-03 20:38:33 -0300

    Add files via upload

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -5,21 +5,76 @@ import java.math.BigDecimal;
 public class AvaliadorAluno {
 
 	public BigDecimal getMaiorNota(BigDecimal[] notas) {
-		// Implementação o código aqui
+		BigDecimal mnota = new BigDecimal(0);
+		if(notas == null)
+			throw new IllegalArgumentException();
+		for(int cont=0; cont<= 3; cont ++) {
+			if (notas[cont].compareTo(mnota) == 1)
+				mnota = notas[cont];
+			}
+			return mnota.setScale(2);
 		
-		return null;
+		
 	}
 
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
+		BigDecimal Media = new BigDecimal("0.0");
+
+		
+		if((nota1!=null && nota2!=null && nota3 != null) &&
+		   (nota1.compareTo(new BigDecimal("0"))>0 && nota2.compareTo(new BigDecimal("0"))>0 && nota3.compareTo(new BigDecimal("0"))>0) &&
+		   (nota1.compareTo(new BigDecimal("10"))<=0 && nota2.compareTo(new BigDecimal("10"))<=0 && nota3.compareTo(new BigDecimal("10"))<=0))
+		{
+			
+			Media = nota1.add(nota2).add(nota3).divide(new BigDecimal("3"),2, BigDecimal.ROUND_HALF_UP);
+						
+			return Media.setScale(2);
+	     }
+		else {
+			
+			throw new IllegalArgumentException();
+		}
+	
+		
+		
+		
+			
 	}
 
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
+		String mensagem;
+		BigDecimal media = getMedia(nota1, nota2, nota3);
+		
+		
+		
+		if((nota1 !=null && nota2 !=null && nota3 != null) &&
+		(nota1.compareTo(new BigDecimal("0"))>0 && nota2.compareTo(new BigDecimal("0"))>0 && nota3.compareTo(new BigDecimal("0"))>0) &&
+		(nota1.compareTo(new BigDecimal("10"))<=0 && nota2.compareTo(new BigDecimal("10"))<=0 && nota3.compareTo(new BigDecimal("10"))<=0)) 
+		 {
+			if(media.compareTo(new BigDecimal("6"))>=1) {
+				mensagem="APROVADO";       }
+		       else
+		    	   if(media.compareTo(new BigDecimal("4"))>=0) {
+		   		mensagem="PROVA_FINAL";       }
+		       
+		       else 
+		    	   mensagem="REPROVADO";    
+			
+			
+			   return mensagem;
+		 }
+		
+		throw new IllegalArgumentException();
+	
+		
 	}
+		
+}
+		
+		
+		
+	
 	
 	
 
-}
+
--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -5,18 +5,70 @@ import java.math.BigDecimal;
 public class CalculadoraSalario {
 
 	public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
-		// Implementar o código aqui
-		return null;
+
+		BigDecimal DescontoINSS = new BigDecimal("0");
+		BigDecimal Salario = new BigDecimal("0");
+		if (percentualImpostoINSS != null && salarioBruto != null) {
+			DescontoINSS = (salarioBruto.multiply(percentualImpostoINSS)).divide(new BigDecimal("100"), 2,
+					BigDecimal.ROUND_HALF_UP);
+			Salario = salarioBruto.subtract(DescontoINSS);
+			return Salario.setScale(2);
+		} else
+			throw new IllegalArgumentException();
+
 	}
 
 	public BigDecimal getValorINSS(BigDecimal salarioBruto) {
-		// Implementar o código aqui
-		return null;
+		BigDecimal ImpostoINSS = new BigDecimal("0");
+
+		if (salarioBruto != null) {
+			if (salarioBruto.compareTo(new BigDecimal("1693.72")) <= 0) {
+				ImpostoINSS = salarioBruto.multiply(new BigDecimal("8")).divide(new BigDecimal("100"), 2,
+						BigDecimal.ROUND_HALF_UP);
+
+			} else if (salarioBruto.compareTo(new BigDecimal("1693.73")) >= 0
+					&& salarioBruto.compareTo(new BigDecimal("2822.90")) <= 0) {
+				ImpostoINSS = salarioBruto.multiply(new BigDecimal("9")).divide(new BigDecimal("100"), 2,
+						BigDecimal.ROUND_HALF_UP);
+
+			} else if (salarioBruto.compareTo(new BigDecimal("2822.91")) >= 0) {
+				ImpostoINSS = salarioBruto.multiply(new BigDecimal("11")).divide(new BigDecimal("100"), 2,
+						BigDecimal.ROUND_HALF_UP);
+			}
+
+			return ImpostoINSS;
+		} else {
+			throw new IllegalArgumentException();
+		}
+
 	}
 
 	public BigDecimal getValorPlanoDeSaude(Integer idade) {
-		// TODO Auto-generated method stub
-		return null;
+
+		if (idade == null && idade <= 0) {
+			throw new IllegalArgumentException();
+		}
+		if (idade >= 0 && idade <= 9) {
+			return new BigDecimal("75").setScale(2);
+
+		} else if (idade >= 10 && idade <= 19) {
+			return new BigDecimal("112.50").setScale(2);
+
+		} else if (idade >= 20 && idade <= 29) {
+			return new BigDecimal("168.75").setScale(2);
+
+		} else if (idade >= 30 && idade <= 39) {
+			return new BigDecimal("253.13").setScale(2);
+
+		} else if (idade >= 40 && idade <= 49) {
+			return new BigDecimal("379.69").setScale(2);
+
+		} else if (idade >= 50 && idade <= 59) {
+			return new BigDecimal("569.54").setScale(2);
+
+		} else {
+			return new BigDecimal("854.30").setScale(2);
+		}
 	}
 
 }
